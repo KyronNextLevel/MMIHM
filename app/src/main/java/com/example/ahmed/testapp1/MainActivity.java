a1be073ba437d9ee1c237bbc6b11372d295249b3
KyronNextLevel <kyron58@protonmail.com>
2018-11-02 02:07:47 +0100

    ajout du projet initial après une énorme galère pour résoudre un caractère en trop dans le gradle.build

---
+++ app/src/main/java/com/example/ahmed/testapp1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.ahmed.testapp1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Intent intent1 = new Intent(MainActivity.this,CarteVisite.class);
+                MainActivity.this.startActivity(intent1);
+                return true;
+                case R.id.navigation_dashboard:
+                    Intent intent2 = new Intent(MainActivity.this,Alarm.class);
+                    MainActivity.this.startActivity(intent2);
+                    return true;
+
+                    case R.id.navigation_notifications:
+                    Intent intent3 = new Intent(MainActivity.this,Notification.class);
+                    MainActivity.this.startActivity(intent3);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
