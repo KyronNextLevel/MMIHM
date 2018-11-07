b30c603a77017b1ff3e011214ef360ce83314e08
KyronNextLevel <kyron58@protonmail.com>
2018-11-07 10:38:43 +0100

    ajout des boutons de raccourcis, de la liste, attention, les activity ne sont pas liées entre elles

--- app/src/main/java/com/example/ahmed/testapp1/MainActivity.java
+++ app/src/main/java/com/example/ahmed/testapp1/MainActivity.java
@@ -6,46 +6,37 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    Intent intent1 = new Intent(MainActivity.this,CarteVisite.class);
-                MainActivity.this.startActivity(intent1);
-                return true;
-                case R.id.navigation_dashboard:
-                    Intent intent2 = new Intent(MainActivity.this,Alarm.class);
-                    MainActivity.this.startActivity(intent2);
-                    return true;
-
-                    case R.id.navigation_notifications:
-                    Intent intent3 = new Intent(MainActivity.this,Notification.class);
-                    MainActivity.this.startActivity(intent3);
-                    return true;
-
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 }
